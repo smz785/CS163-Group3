3ca82b33b5c8f52339510bd00821e74d35320833
smz785 <20480538@fhda.edu>
2026-03-21 12:30:06 -0700

    Updated app.py and setup gcloud bucket

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ import plotly.express as px
 from google.cloud import storage
 import os
 from io import StringIO
+import functools
 
 
 # BLOB is an acronym for "Binary Large Object". It's a data type that stores binary data, such as images, videos, and audio.
-def get_csv_from_gcs(bucket_name, source_blob_name):
+@functools.lru_cache(maxsize = 1)
+def get_csv_from_gcs(bucket_name, source_blob_name, n_rows = 10000):
     """Downloads a blob from the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
 
-    # The ID of your GCS object
-    # source_blob_name = "storage-object-name"
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
 
-    # Construct a client side representation of a blob.
-    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
-    # any content from Google Cloud Storage. As we don't need additional data,
-    # using `Bucket.blob` is preferred here.
+
     blob = bucket.blob(source_blob_name)
-    data = blob.download_as_text()
-    return pd.read_csv(StringIO(data))
+    with blob.open("r") as f:
+        return pd.read_csv(f)
 
 
 # Initialize the app
@@ -39,19 +34,21 @@ server = app.server
 
 # Cloud Storage demo
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
-df = get_csv_from_gcs(BUCKET_NAME, 'criteo-uplift-v2.1')
+df = get_csv_from_gcs(BUCKET_NAME, 'criteo-uplift-v2.1.csv')
+print(df)
+
 
 # App layout
 app.layout = html.Div([
-    html.Div(children=''),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph'),
-    html.Hr(),
-    # this is a separate table for google cloud storage demo
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6)
-])
+     html.Div(children=''),
+#     html.Hr(),
+#     dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+#     dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+#     dcc.Graph(figure={}, id='controls-and-graph'),
+#     html.Hr(),
+#     # this is a separate table for google cloud storage demo
+     dash_table.DataTable(data=df.to_dict('records'), page_size=6)
+ ])
 
 
 
