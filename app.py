3359cc6b53c4f3441d67d4b28b29ce32dc688ae9
smz785 <20480538@fhda.edu>
2026-04-10 12:23:12 -0700

    Modified the app into a multi-page dash app.
    Working on Analytics page now

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 # Import packages
-from dash import Dash, html
-
+import dash
 # Optional / future imports (uncomment when used):
-# from dash import dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dcc, dash_table, dcc, callback, Output, Input
 # import pandas as pd
 # import plotly.express as px
 # from google.cloud import storage
@@ -10,34 +9,26 @@ from dash import Dash, html
 # from io import StringIO
 # import functools
 
-from flask import send_from_directory
-
-
-# Initialize the app
-# Run Dash under the '/dash/' path so the Flask root ('/') can serve the static HTML page.
-app = Dash(__name__, routes_pathname_prefix='/dash/', requests_pathname_prefix='/dash/')
-
-# the underlying Flask server instance that Dash uses to run the application
-# Many WSGI servers (e.g., Gunicorn, uWSGI) expect a server object.
-# https://dash.plotly.com/deployment
-server = app.server
-
+app = Dash(__name__, use_pages=True)
 
 # Minimal Dash layout so the Dash endpoints are available.
 app.layout = html.Div([
-    html.H1("Dash app running (mounted at /dash/ )"),
-    html.P("Click below to open the static CS163 webpage served at the root URL."),
-    html.A("Open CS163_Webpage.html (root)", href='/', target='_self'),
-    html.Div(style={'height': '1rem'}),
-    html.P("Or visit the Dash app directly at /dash/")
+
+    html.Header([
+        html.Div("CS 163 - Group 3", className = "course-tag"),
+        html.H1("Criteo Uplift Data Modeling and Analysis"),
+        html.P("Predicting Treatment Effect on User Conversions Using Machine Learning")
+    ]),
+
+    html.Nav([
+        dcc.Link("Overview", href='/'),
+        dcc.Link("Dataset", href='/dataset'),
+        dcc.Link("Methods", href="/methods"),
+        dcc.Link("Analytics", href="/analytics")
+    ]),
+
+    html.Footer("CS 163 &mdash; Group 3 &bull; Spring 2026 &bull; Built with HTML &amp; CSS")
 ])
-
-
-# Serve the standalone HTML file from the 'static' folder at the root URL.
-@server.route('/')
-def index():
-    return send_from_directory('static', 'CS163_Webpage.html')
-
 
 # Run the app
 if __name__ == '__main__':
