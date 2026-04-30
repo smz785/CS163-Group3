146dffcaa6dcad68b651d6bbaed2f8908531c779
smz785 <20480538@fhda.edu>
2026-04-30 14:35:51 -0700

    updated data_store.py to load dataset from google cloud bucket updated EDA.py to use df from data_store.py

--- data_store.py
+++ data_store.py
@@ -2,16 +2,21 @@ import os
 import functools
 import pandas as pd
 from google.cloud import storage
-from io import StringIO
+from io import BytesIO
+
+#----- Converted the cvs file to parquet and uploaded it to GCS bucket -------------
+# df = pd.read_csv("C:/Users/SyedZain/Downloads/criteo-uplift-v2.1.csv/criteo-uplift-v2.1.csv")
+#
+# df.to_parquet('C:/Users/SyedZain/Desktop/criteo-uplift-v2.1.parquet')
 
 BUCKET_NAME = os.environ.get("BUCKET_NAME", 'group-3-bucket')
-FILE_NAME = "criteo-uplift-v2.1.csv"
+FILE_NAME = "criteo-uplift-v2.1.parquet"
 
 @functools.lru_cache(maxsize=1)
 def get_df():
     print(f" loading data from gcs://{BUCKET_NAME}/{FILE_NAME} ")
     client = storage.Client()
     blob_data = client.bucket(BUCKET_NAME).blob(FILE_NAME)
-    data = blob_data.download_as_text()
-    df = pd.read_csv(StringIO(data))
+    data = blob_data.download_as_bytes()
+    df = pd.read_parquet(BytesIO(data))
     return df
